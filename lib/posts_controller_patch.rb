777875f9f9a28800157c7d5571c37dfa7249fb27
Zach Hamman <z.hamman@gmail.com>
2014-08-31 15:20:56 -0400

    fix comment counts

--- lib/posts_controller_patch.rb
+++ lib/posts_controller_patch.rb
@@ -7,5 +7,17 @@ module PostsControllerPatch
   
   included do
     helper PostsHelper
+    remove_method :show
+    def show
+    @post = if params[:slug] && params[:year] && params[:month]
+      @blog.posts.published.where(:year => params[:year], :month => params[:month], :slug => params[:slug]).first!
+    else
+      @blog.posts.published.where(:slug => params[:slug]).first!
+    end
+    @comment = Comfy::Blog::Comment.new({post_id: @post.id})
+
+  rescue ActiveRecord::RecordNotFound
+    render :cms_page => '/404', :status => 404
+  end
   end
 end
