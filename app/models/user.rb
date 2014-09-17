6851e71306f8daeaeb53878f348d30f643b163bf
Zach Hamman <z.hamman@gmail.com>
2014-09-17 18:11:52 -0400

    remove unneeded method from user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -73,10 +73,6 @@ class User < ActiveRecord::Base
     now = Time.now.utc.to_date
     now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
   end
-  
-  def generate_token
-   SecureRandom.urlsafe_base64
-  end
 end
 
 
