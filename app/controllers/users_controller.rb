6c34bf08972afbe45a42b20b1874fc01b7e6f1e5
Zach Hamman <z.hamman@gmail.com>
2014-10-06 10:46:05 -0400

    use logintoken when registering new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,9 +25,9 @@ class UsersController < ApplicationController
       @user.assign_attributes(user_params)
       @user.permission=User::STATUS_MEMBER
       @user.password,@user.password_confirmation=params[:user][:password],params[:user][:confirm]
-      @user.generate_token(:auth_token)
       if @user.save
-        cookies[:auth_token]=@user.auth_token
+        lt = LoginToken.create!(:user=>@user,:permanent=>false)
+        cookies[:auth_token]=lt.token
         @user.update_column(:last_login, Time.now)
         @user.update_column(:updated_at, Time.now)
         redirect_to profile_path
