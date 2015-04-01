7a888fd1c5880f0806d96c5e3e3043119fcd561e
Zach Hamman <zhamman@taximagic.com>
2015-04-01 06:35:46 -0400

    lower cache time, delete cache on commit; change dimensions for image

--- app/models/month_athlete.rb
+++ app/models/month_athlete.rb
@@ -1,12 +1,20 @@
 class MonthAthlete < ActiveRecord::Base
   belongs_to :picture, :class=>Comfy::Cms::File
+  
+  after_commit :clear_cache
 
   def self.get_latest
     result = Rails.cache.read(:aotm)
     if result.nil?
       result = MonthAthlete.where(:month=>Date.today.month, :year=>Date.today.year).first
-      Rails.cache.write(:aotm, result, :expires=>1.day.from_now)
+      Rails.cache.write(:aotm, result, :expires=>5.minutes.from_now)
     end
     result
+  end
+  
+  private
+  
+  def clear_cache
+    Rails.cache.delete(:aotm)
   end
 end
