9187dce03959e04e63bbe7562ab404118c4cea08
Zach Hamman <zhamman@taximagic.com>
2015-05-27 06:14:19 -0400

    change session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Baltbear::Application.config.session_store :cache_store, key: '_baltbear_session', expire_after: 2.hours
+require 'action_dispatch/middleware/session/dalli_store'
+Baltbear::Application.config.session_store :dalli_store, key: '_baltbear_session', expire_after: 2.hours
